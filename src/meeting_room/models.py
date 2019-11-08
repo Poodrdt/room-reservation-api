f053995b268ca200d455592c4fad660b1f002972
Yaroslav Koval <dfmdeveloper10@lyb.it>
2019-11-08 19:35:21 +0200

    going home commit

--- src/meeting_room/models.py
+++ src/meeting_room/models.py
@@ -7,10 +7,12 @@ class Employee(User):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 class Room():
+    name = models.CharField(max_length=255)
     def is_occupied(self, time):
         return True
 
 class Reservation():
+    title = models.CharField(max_length=255)
     emploee = models.ForeignKey(Employee)
     room = models.ForeignKey(Room)
     start = models.DateTimeField()
